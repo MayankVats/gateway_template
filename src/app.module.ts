5a3242c93275318f4cbdad4a5738b166baab6a37
MayankVats <vats.2610@gmail.com>
2023-08-28 15:58:46 +0530

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { IntrospectAndCompose } from '@apollo/gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        // TODO: Make it environment specific
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://127.0.0.1:8080/graphql',
+            },
+            {
+              name: 'posts',
+              url: 'http://127.0.0.1:8081/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService).get();
+
+  await app.listen(config.PORT, () => {
+    console.log('ENVIRONMENT LOADED: ', config);
+    console.log('Server running at PORT ->', config.PORT);
+  });
 }
 bootstrap();
